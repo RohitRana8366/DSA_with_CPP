3ac8ba4024b342e37b4611dd7affb1232e6405ae
RohitRana8366 <rrohit8366@gmail.com>
2025-12-18 10:50:10 +0530

    For understanding c++ selection sort.

---
+++ selectionsort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int arr[n];
+
+    cout << "Enter array five elements:\n";
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
