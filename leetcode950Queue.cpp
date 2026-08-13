4d3334830344beaf218539ce201ade7362b6f7b5
RohitRana8366 <rrohit8366@gmail.com>
2026-08-13 18:39:03 +0530

    solve reveal cards problem of queue data structure.

---
+++ leetcode950Queue.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        sort(deck.begin(),deck.end());
+        queue<int>q;
+        for(int i=0; i<deck.size(); i++){
+            q.push(i);
+        }
+        vector<int>ans(deck.size());
+        for(int i=0; i<deck.size(); i++){
+            int index=q.front();
+            q.pop();
+            ans[index]=deck[i];
+
+
+            if(!q.empty()){
+                q.push(q.front());
+                q.pop();
+
+            }
+
+
+        }
+        return ans;
+    }
+};
